820095fb45fe88eff4596be55b17056ce4999253
Kirsty <congokirsty@gmail.com>
2025-01-20 20:19:57 +0000

    completes autocompleteSearch and typedLocation and returns location on the map

--- utils/networkValidation.js
+++ utils/networkValidation.js
@@ -2,6 +2,6 @@ import * as Network from 'expo-network';
 
 export const checkConnection = async () => {
     const networkState = await Network.getNetworkStateAsync();
-    console.log(networkState)//debugging***
+        console.log(networkState)//debugging***
     return networkState.isConnected && networkState.isInternetReachable;
 }
